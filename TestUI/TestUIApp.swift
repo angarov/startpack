26d3533445bd9a5ff3e1f4abd5da3b390bb7d668
Leonid <langarov@tripactions.com>
2022-05-23 16:16:59 +0300

    init

---
+++ TestUI/TestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUIApp.swift
+//  TestUI
+//
+//  Created by Leonid Angarov on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
